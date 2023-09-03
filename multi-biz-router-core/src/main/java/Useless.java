a0a17b64d81616e2d3725948161cb96095f3b0c6
topicstudy <topicstudy@163.com>
2023-09-03 12:48:16 +0800

    init project

---
+++ multi-biz-router-core/src/main/java/Useless.java
@@ -0,0 +1,8 @@
+public class Useless {
+    /**
+     * In order to pack out javadoc.jar
+     * It will do not package out javadoc.jar, if no Java file included
+     */
+    public void foo() {
+    }
+}
